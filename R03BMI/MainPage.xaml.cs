859e225e04359a919ec2133248c096aa1b88666c
柊輔 佐藤 <190676@jc-21.jp>
2021-11-16 12:39:16 +0900

    bmiを計算する処理とBMI_Checkメソッドを作成

--- R03BMI/MainPage.xaml.cs
+++ R03BMI/MainPage.xaml.cs
@@ -18,7 +18,53 @@ namespace R03BMI
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            double height = double.Parse(heighit.Text); //身長
+            double weight = double.Parse(heighit.Text); //体重
 
+            if(height > 3)  //cmをｍに変換
+            {  
+                height /= 100;
+            }
+            
+            if(weight > 600) //gをkgに変換
+            {
+                weight/=1000;
+            }
+
+            double bmi =weight / (height*height);
+            bmi /= Math.Round(bmi,2);
+
+            string BMIStatus = BMI_Check(bmi); 
+
+            result.Text = bmi +" : "+ BMIStatus;
+
+        }
+
+        private string BMI_Check(bouble bmi)    //bmiを元に体の状態(例：普通体重,低体重)を返す
+        {
+            string BMIStatus;
+
+            if(bmi < 18.5)
+            {
+                BMIStatus = "低体重(痩せ)";
+            }else if(bmi < 25.0)
+            {
+                BMIStatus = "普通体重";
+            }else if(bmi < 30.0)
+            {
+                BMIStatus = "肥満(1度)";
+            }else if(bmi < 35.0)
+            {
+                BMIStatus = "肥満(2度)";
+            }else if(bmi < 40)
+            {
+                BMIStatus = "肥満(3度)";
+            }else
+            {
+                BMIStatus = "肥満(4度)";
+            }
+
+            return BMIStatus;
         }
     }
 }
